d8db0d54fbaa83cfd5778d1111e120956dbb271d
Sulthan-Sulthan <farishf24@gmail.com>
2024-06-09 11:23:15 +0530

    divisible by k leetcode problem

---
+++ subArrayDivByK.cpp
@@ -0,0 +1,10 @@
+vector<int> counts(K, 0);
+        int sum = 0;
+        for(int x: A){
+            sum += (x%K + K)%K;
+            counts[sum % K]++;
+        }
+        int result = counts[0];
+        for(int c : counts)
+            result += (c*(c-1))/2;
+        return result;
